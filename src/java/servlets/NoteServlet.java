bc5f330c63c3a78656079283d093a42d4796e584
821214 <821214@SAIT225178IT.ACDM.DS.SAIT.CA>
2021-02-06 18:51:01 -0700

    Imported Buffer Reader & Print Writer. Started buffer reader and print writer to read and write to the text file

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -1,7 +1,13 @@
 
 package servlets;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +24,20 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-     
+        
+        
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+// to read files
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+// to write to a file
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false)));
+        
+        String title = br.readLine();
+        String content = br.readLine();
+        
+        br.close();
+        Note note = new Note(title, content);
+        request.setAttribute("note", note);
       /*  
         if (edit == true){
         getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp")
